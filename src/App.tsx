764b10d7033684f8a292eee7b628149d5dccaf21
Vasu Garg <vasugargup@gmail.com>
2022-06-21 14:17:19 +0530

    Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
     return (
-        <div>
-
+        <div className={'App-container'}>
+            <div className='layout'>
+                <Header/>
+            </div>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return <header className='header'>
+        <h4>
+            Parking Lot Management
+        </h4>
+    </header>
+}
+
+export default Header;
